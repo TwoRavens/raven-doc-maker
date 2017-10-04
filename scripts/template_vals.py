080ff77351623ea8c48b4508ec6992096f187bc6
Raman Prasad <raman_prasad@harvard.edu>
2017-10-04 00:48:35 -0400

    update

--- scripts/template_vals.py
+++ scripts/template_vals.py
@@ -5,43 +5,43 @@ TWORAVENS_REGISTRY = '%s/tworavens/tworavens' % D3M_REGISTRY
 
 # Refers to containers in gitlab.datadrivendiscovery.org registries
 #
-ravens_nginx_image_key = 'ravens_nginx_image'
-tworavens_image_key = 'tworavens_image'
-rook_image_key = 'rook_image'
-isi_ta2_image_key = 'isi_ta2_image'
-cra_ta2_image_key = 'cra_ta2_image'
+RAVENS_NGINX_IMAGE_KEY = 'ravens_nginx_image'
+TWORAVENS_IMAGE_KEY = 'tworavens_image'
+ROOK_IMAGE_KEY = 'rook_image'
+ISI_TA2_IMAGE_KEY = 'isi_ta2_image'
+CRA_TA2_IMAGE_KEY = 'cra_ta2_image'
 
-DEPLOY_DICT = {ravens_nginx_image_key : '%s/nginx:latest' % TWORAVENS_REGISTRY,
-               tworavens_image_key : '%s/tworavens:latest' % TWORAVENS_REGISTRY,
-               rook_image_key : '%s/rook-service:latest' % TWORAVENS_REGISTRY,
+DEPLOY_DICT = {RAVENS_NGINX_IMAGE_KEY : '%s/nginx:latest' % TWORAVENS_REGISTRY,
+               TWORAVENS_IMAGE_KEY : '%s/tworavens:latest' % TWORAVENS_REGISTRY,
+               ROOK_IMAGE_KEY : '%s/rook-service:latest' % TWORAVENS_REGISTRY,
                # leave in for NIST
                'CONFIG_JSON_PATH' : '{{ CONFIG_JSON_PATH }}',
                'eval_id' : '{{ eval_id }}',
                # ISI specific
-               isi_ta2_image_key : '%s/ta2/isi_ta2:python3' % D3M_REGISTRY,
+               ISI_TA2_IMAGE_KEY : '%s/ta2/isi_ta2:python3' % D3M_REGISTRY,
                'RAVENS_ISI_VOLUME_MOUNTS' : RAVENS_ISI_VOLUME_MOUNTS,
                # CRA specific
-               cra_ta2_image_key : '%s/eve/docker-images:latest' % D3M_REGISTRY}
+               CRA_TA2_IMAGE_KEY : '%s/eve/docker-images:latest' % D3M_REGISTRY}
 
 # Using tags given to containers after download
 #
-MINIKUBE_DICT = {ravens_nginx_image_key : 'ravens_nginx:latest',
-                 tworavens_image_key : 'tworavens:latest',
-                 rook_image_key : 'rook:latest',
+MINIKUBE_DICT = {RAVENS_NGINX_IMAGE_KEY : 'ravens_nginx:latest',
+                 TWORAVENS_IMAGE_KEY : 'tworavens:latest',
+                 ROOK_IMAGE_KEY : 'rook:latest',
                  # Fill in
                  'CONFIG_JSON_PATH' : '/ravens_volume/config_o_196.json',
                  'eval_id' : 'ravens-eval',
                  # ISI specific
-                 isi_ta2_image_key : 'isi_ta2:latest',
+                 ISI_TA2_IMAGE_KEY : 'isi_ta2:latest',
                  'RAVENS_ISI_VOLUME_MOUNTS' : RAVENS_ISI_VOLUME_MOUNTS,
                  # CRA specific
-                 cra_ta2_image_key : 'cra_ta2:latest'}
+                 CRA_TA2_IMAGE_KEY : 'cra_ta2:latest'}
 
-TA3_IMAGE_NAME_KEYS = [ravens_nginx_image_key, tworavens_image_key, rook_image_key]
+TA3_IMAGE_NAME_KEYS = [RAVENS_NGINX_IMAGE_KEY, TWORAVENS_IMAGE_KEY, ROOK_IMAGE_KEY]
 TA3_IMAGE_NAMES = [DEPLOY_DICT[k] for k in TA3_IMAGE_NAME_KEYS]
 
-ISI_IMAGE_NAME = DEPLOY_DICT[isi_ta2_image_key]
-CRA_IMAGE_NAME = DEPLOY_DICT[cra_ta2_image_key]
+ISI_IMAGE_NAME = DEPLOY_DICT[ISI_TA2_IMAGE_KEY]
+CRA_IMAGE_NAME = DEPLOY_DICT[CRA_TA2_IMAGE_KEY]
 
 # docker pull commands
 #
